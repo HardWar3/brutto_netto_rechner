522ab34be49049e7dfe8e3fc3df788a051708eed
HardWar3 <mikewegner@gmx.net>
2024-09-18 20:37:24 +0200

    add functions

--- classes/Lohnsteuer_2024DEC.cs
+++ classes/Lohnsteuer_2024DEC.cs
@@ -465,6 +465,267 @@ namespace brutto_netto_rechner.classes
                 }
             }
         }
+        private void Msolz()
+        {
+            Solzfrei *= Kztab;
+            if (Jbmg > Solzfrei)
+            {
+                // TODO abrunden auf ganze cents
+                Solzj = Jbmg * 5.5m / 100m;
+                Solzmin = (Jbmg - Solzfrei) * 11.9m / 100m;
+                if (Solzmin < Solzj)
+                {
+                    Solzj = Solzmin;
+                }
+                Jw = Solzj * 100m;
+                if (Schleifz == 1)
+                {
+                    Jwbka = Jw;
+                } else
+                {
+                    Jwbkn = Jw;
+                }
+                Upanteil();
+                Solzlzz = Anteil1;
+            } else
+            {
+                Solzlzz = decimal.Zero;
+            }
+            if (R > 0)
+            {
+                Jw = Jbmg * 100m;
+                if (Schleifz == 1)
+                {
+                    Jwbka = Jw;
+                } else
+                {
+                    Jwbkn = Jw;
+                }
+                Upanteil();
+                Bk = Anteil1;
+            } else
+            {
+                Bk = 0;
+            }
+        }
+        private void Mlst1224()
+        {
+            if (Lzz > 1)
+            {
+                Jw = Jwlstn - 11m * (Jwlsta - Jwlstn);
+                if (Jw < 0)
+                {
+                    Anteil1 = decimal.Zero;
+                } else
+                {
+                    Upanteil();
+                }
+                Lstlzz = Anteil1;
+                Jw = Jwsolzn - 11m * (Jwsolza - Jwsolzn);
+                if (Jw < 0)
+                {
+                    Anteil1 = decimal.Zero;
+                } else
+                {
+                    Upanteil();
+                }
+                Solzlzz = Anteil1;
+                Jw = Jwbkn - 11 * (Jwbka - Jwbkn);
+                if (Jw < 0)
+                {
+                    Anteil1 = decimal.Zero;
+                } else
+                {
+                    Upanteil();
+                }
+                Bk = Anteil1;
+            }
+        }
+        private void Msonst()
+        {
+            Lzz = 1;
+            if (Zmvb == 0)
+            {
+                Zmvb = 12;
+            }
+            if (Sonstb == 0 && Mbv == 0)
+            {
+                Vkvsonst = decimal.Zero;
+                Lstso = decimal.Zero;
+                Sts = decimal.Zero;
+                Solzs = decimal.Zero;
+                Bks = decimal.Zero;
+            } else
+            {
+                Mosonst();
+                Upvkv();
+                Vkvsonst = Vkv;
+                Zre4j = (Jre4 + Sonstb) / 100m;
+                Zvbezj = (Jvbez + Vbs) / 100m;
+                Vbezbso = Sterbe;
+                Mre4sonst();
+                Mlstjahr();
+                Wvfrbm = (Zve - Gfb) * 100m;
+                if (Wvfrbm < 0)
+                {
+                    Wvfrbm = decimal.Zero;
+                }
+                Upvkv();
+                Vkvsonst = Vkv - Vkvsonst;
+                Lstso = St * 100m;
+                Sts = (Lstso - Lstoso) * F;
+                Stsmin();
+            }
+        }
+        private void Msolzsts()
+        {
+            if (Zkf > 0)
+            {
+                Solzszve = Zve - Kfb;
+            } else
+            {
+                Solzszve = Zve;
+            }
+            if (Solzszve < 1)
+            {
+                Solzszve += decimal.Zero;
+                X =  decimal.Zero;
+            } else
+            {
+                // TODO abrunden auf ganze euro
+                X = Solzszve / Kztab;
+            }
+            if (Stkl < 5)
+            {
+                Uptab24n();
+            } else
+            {
+                Mst5_6();
+            }
+            // TODO abrunden auf ganze euro
+            Solzsbmg = St * F;
+            if (Solzsbmg > Solzfrei)
+            {
+                // TODO abrunden auf ganze cents
+                Solzs = Sts * 5.5m / 100m;
+            }
+            else
+            {
+                Solzs = decimal.Zero;
+            }
+        }
+        private void Mvmt()
+        {
+            if (Vkapa < 0)
+            {
+                Vkapa = decimal.Zero;
+            }
+            if (Vmt + Vkapa > 0)
+            {
+                if (Lstso == 0)
+                {
+                    Mosonst();
+                    Lst1 = Lstoso;
+                } else
+                {
+                    Lst1 = Lstso;
+                }
+                // TODO 39
+            } else
+            {
+                Stv = decimal.Zero;
+                Solzv = decimal.Zero;
+                Bkv = decimal.Zero;
+            }
+        }
+        private void Stsmin()
+        {
+            if (Sts < 0)
+            {
+                if (Mbv != 0)
+                {
+                    Lstlzz += Sts;
+                    if (Lstlzz < 0)
+                    {
+                        Lstlzz = decimal.Zero;
+                    }
+                    // TODO abrunden auf ganze cents
+                    Solzlzz += Sts * 5.5m / 100m;
+                    if (Solzlzz < 0)
+                    {
+                        Solzlzz = decimal.Zero;
+                    }
+                    Bk += Sts;
+                    if (Bk < 0)
+                    {
+                        Bk = decimal.Zero;
+                    }
+                }
+                Sts = decimal.Zero;
+                Solzs = decimal.Zero;
+            } else
+            {
+                Msolzsts();
+            }
+            if (R > 0)
+            {
+                Bks = Sts;
+            } else
+            {
+                Bks = decimal.Zero;
+            }
+        }
+        private void Upanteil()
+        {
+            switch (Lzz)
+            {
+                case 1:
+                    Anteil1 = Jw;
+                    break;
+                case 2:
+                    // TODO ergebnis aufrunden
+                    Anteil1 = Jw / 12m;
+                    break;
+                case 3:
+                    // TODO ergebnis aufrunden
+                    Anteil1 = Jw * 7m / 360m;
+                    break;
+                default:
+                    // TODO ergebnis aufrunden
+                    Anteil1 = Jw / 360m;
+                    break;
+            }
+        }
+        private void Up5_6()
+        {
+            X = Zx * 1.25m;
+            if (Schleifz == 1)
+            {
+                Uptab24a();
+            } else
+            {
+                Uptab24n();
+            }
+            St1 = St;
+            X = Zx * 0.75m;
+            if (Schleifz ==  1)
+            {
+                Uptab24a();
+            } else
+            {
+                Uptab24n();
+            }
+            St2 = St;
+            Diff = (St1 - St2) * 2m;
+            Mist = Zx * 0.14m;
+            if (Mist > Diff)
+            {
+                St = Mist;
+            } else
+            {
+                St = Diff;
+            }
+        }
         private void Upvkvlzz()
         {
             Upvkv();
